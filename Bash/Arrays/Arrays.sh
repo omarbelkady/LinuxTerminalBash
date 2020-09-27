588142c84b3902af557c4e2d6033c78f6e2a069c
Omar <omar.belkady@g.austincc.edu>
2020-09-27 21:03:39 +0100

    Arrays In Bash

---
+++ Bash/Arrays/Arrays.sh
@@ -0,0 +1,9 @@
+#! /data/data/com.termux/files/usr/bin/bash
+#Unlimited inputs
+
+arguments=("$@")
+
+#echo ${arguments[0]} ${arguments[1]} ${arguments[2]} ${arguments[3]} ${arguments[4]}
+
+#Unlimited Inps Correctly
+echo $@
